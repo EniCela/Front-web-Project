e2b6dc3f7ed7b68f5429b61b9994a029a1094126
Sergei <tolulisergei@gmail.com>
2023-03-12 18:17:00 +0100

    Initial commit in frontend

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import { FormsModule ,ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,   
+     FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
